b5fb39813e479f03d103aa2704b16829ce0a29bc
jbarrag3 <jbarrag3@its.jnj.com>
2022-08-05 16:25:25 -0700

    save scheduler state dict

--- pytorchcheckpoint/checkpoint.py
+++ pytorchcheckpoint/checkpoint.py
@@ -73,7 +73,9 @@ class CheckpointHandler:
         checkpoint_path = os.path.join(path2save, filename)
         return checkpoint_path
 
-    def save_checkpoint(self, checkpoint_path, iteration, batch_count, model, optimizer):
+    def save_checkpoint(
+        self, checkpoint_path, iteration, batch_count, model, optimizer, scheduler=None
+    ):
         if type(model) == torch.nn.DataParallel:
             # converting a DataParallel model to be able load later without DataParallel
             self.model_state_dict = model.module.state_dict()
@@ -81,6 +83,11 @@ class CheckpointHandler:
             self.model_state_dict = model.state_dict()
 
         self.optimizer_state_dict = optimizer.state_dict()
+        if scheduler is not None:
+            self.scheduler_state_dict = scheduler.state_dict()
+        else:
+            self.scheduler_state_dict = None
+
         self.iteration = iteration
         self.batch_count = batch_count
         torch.save(self, checkpoint_path)
@@ -91,10 +98,17 @@ class CheckpointHandler:
         return checkpoint
 
     @staticmethod
-    def load_checkpoint_with_model(checkpoint_path, model=None, optimizer=None, map_location="cpu"):
+    def load_checkpoint_with_model(
+        checkpoint_path, model=None, optimizer=None, scheduler=None, map_location="cpu"
+    ):
         checkpoint = torch.load(checkpoint_path, map_location=map_location)
         if model is not None:
             model.load_state_dict(checkpoint.model_state_dict)
         if optimizer is not None:
             optimizer.load_state_dict(checkpoint.optimizer_state_dict)
-        return checkpoint, model, optimizer
+        if scheduler is not None:
+            if checkpoint.scheduler_state_dict is not None:
+                scheduler.load_state_dict(checkpoint.scheduler_state_dict)
+            else:
+                raise Exception("Non scheduler state dict available")
+        return checkpoint, model, optimizer, scheduler
